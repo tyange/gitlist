bfd9c4c252a48056d38da4f60585d5f57415836a
babyazalea <usun12@hotmail.com>
2021-07-27 13:18:30 +0900

    basic routing & re-styling

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MyListComponent } from './my-list/my-list.component';
+import { ReposComponent } from './repos/repos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'repos', component: ReposComponent },
+  { path: 'my-list', component: MyListComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { MyListComponent } from './my-list/my-list.component';
 import { RepoListComponent } from './repos/repo-list/repo-list.component';
 import { RepoComponent } from './repos/repo-list/repo/repo.component';
 import { ReposComponent } from './repos/repos.component';
@@ -12,9 +14,11 @@ import { ReposComponent } from './repos/repos.component';
   declarations: [
     AppComponent,
     HeaderComponent,
-    ReposComponent,
+    HomeComponent,
     RepoListComponent,
     RepoComponent,
+    ReposComponent,
+    MyListComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
