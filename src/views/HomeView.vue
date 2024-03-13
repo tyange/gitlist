0452328430f396394121a34e37e5281b9c6c1fa7
tyange <usun16@gmail.com>
2024-03-13 22:59:23 +0900

    init

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,13 +1,9 @@
-<script setup lang="ts">
-
-</script>
+<script setup lang="ts"></script>
 
 <template>
-<div>
-  <p class="text-red-500">Home</p>
-</div>
+  <div>
+    <p class="text-red-500">Home</p>
+  </div>
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
